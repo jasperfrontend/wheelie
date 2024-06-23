a0ea7f984135d4c468d307b32a5ba4b23b5444f0
Jasper van der Meer <jaspervdmeer@gmail.com>
2024-06-23 15:14:43 +0200

    Integrated YouTube Data API

--- bot.js
+++ bot.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const tmi = require('tmi.js');
 const { createClient } = require('@supabase/supabase-js');
 const express = require('express');
+const youtubeCleaner = require('./lib/theUnpunishedYTCleaner');
+const moment = require('moment');
+require('moment-duration-format');
 
 const PORT = process.env.PORT || 10000;
 
@@ -11,7 +14,7 @@ const opts = {
     username: process.env.TWITCH_BOT_USERNAME,
     password: process.env.TWITCH_OAUTH_TOKEN
   },
-  channels: [process.env.TWITCH_CHANNEL],
+  channels: [],
   connection: {
     reconnect: true,
     secure: true
@@ -19,7 +22,7 @@ const opts = {
 };
 
 // List of authorized users
-const authorizedUsers = ['jasperdiscovers'];
+const authorizedUsers = ['JasperDiscovers'];
 
 // Create a client with our options
 const client = new tmi.Client(opts);
@@ -54,20 +57,20 @@ async function initializeBot() {
     supabase.auth.setSession(session.access_token);
   }
 
-  // Fetch the channels from the database where is_player is true
-  // async function fetchPlayerChannels() {
-  //   const { data, error } = await supabase
-  //     .from('players')
-  //     .select('name')
-  //     .eq('is_player', true);
+  // Fetch the players
+  async function fetchPlayerChannels() {
+    const { data, error } = await supabase
+      .from('players')
+      .select('name')
+      .eq('uid', 'ea04bc6c-39dd-44a5-a9b3-11c42ae2ddf1');
 
-  //   if (error) {
-  //     console.error('Error fetching players.');
-  //     return [];
-  //   }
-
-  //   return data.map(user => user.name);
-  // }
+    if (error) {
+      console.error('Error fetching players.');
+      return [];
+    }
+    // console.log(data.map(user => user.name));
+    return data.map(user => user.name);
+  }
 
   // Register our event handlers 
   client.on('message', onMessageHandler);
@@ -78,8 +81,8 @@ async function initializeBot() {
   client.on('error', onErrorHandler);
 
   // Connect to Twitch and fetch channels
-  // const channels = await fetchPlayerChannels();
-  // client.opts.channels = channels;
+  const channels = await fetchPlayerChannels();
+  client.opts.channels = channels;
   client.connect().catch(console.error);
 }
 
@@ -89,26 +92,23 @@ initializeBot().catch(console.error);
 // Called every time a message comes in
 function onMessageHandler(channel, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
-  // console.log(context); // this is a fuckton of data lol
+
+  // console.log(`* * * context: ${JSON.stringify(context)}. msg: ${msg} * * *`);
+
   // Get the username of the message sender
-  const username = context.username;
+  const username = context['display-name'];
 
   // Check if the user is authorized
-  if (!authorizedUsers.includes(username.toLowerCase())) {
+  if (!authorizedUsers.includes(username)) {
     return;
   }
 
-  // Remove whitespace from chat message and convert to lowercase
   const commandName = msg.trim().toLowerCase();
 
-  // Log the received command for debugging
-  // console.log(`Received command: ${commandName} from ${username}`);
-
-  // If the command is known, let's execute it
-  if (commandName.startsWith('!join')) {
+  if (commandName === '!join') {
     handleJoin(channel, commandName, context);
-  } else if (commandName.startsWith('!sr')) {
-    handleSR(channel, commandName, context);
+  } else if (commandName.startsWith('!r ')) {
+    handleR(channel, commandName, context, msg);
   } else if (commandName === '1' || commandName === '2') {
     handleVote(channel, commandName, context, 'vote');
   } else if (commandName === '69') {
@@ -132,15 +132,13 @@ function onMessageHandler(channel, context, msg, self) {
   } else if (commandName.startsWith('!cat')) { // !cat <category name or partial>
     handleCat(channel, commandName, context);
   } else if (commandName === '!beep') {
-    console.log('!beep received');
-    client.say(channel, 'boop!');
+    handleBeep(channel, commandName, context);
   } 
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler(addr, port) {
-  console.log(`* Connected to [${addr}:${port}]`);
-  // client.say('jasperdiscovers', 'Spinge Bot is alive. Hi!');
+  console.log(`* * * Connected to [${addr}:${port}] * * *`);
 }
 
 // Called every time the bot disconnects from Twitch chat
@@ -152,7 +150,6 @@ function onDisconnectedHandler(reason) {
 // Called every time the bot attempts to reconnect
 function onReconnectHandler() {
   console.log('Reconnecting...');
-  client.say('jasperdiscovers', 'Reconnecting... Twitch smol startup sometimes cannot keep their chatbox online.');
 }
 
 // Called for any notice events from Twitch
@@ -163,48 +160,123 @@ function onNoticeHandler(channel, msgid, message) {
 // Called for any errors
 function onErrorHandler(err) {
   console.error('Error:', err);
-  client.say('jasperdiscovers', 'Something went wrong: ' + err);
+  client.say(process.env.TWITCH_CHANNEL, 'Something went wrong LUL fuck knows what went wrong. This message is literally a last-ditch effort to try and save the bot from disconnecting.');
 }
 
+// Cleans input to remove anything in between [brackets] (brackets) or {brackets}
+function cleanTitle(title) {
+  // Regular expression to match anything in parentheses, square brackets, or curly braces
+  const regex = / *\([^)]*\)| *\[[^\]]*\]| *\{[^}]*\}/g;
+  // Replace matched content with an empty string
+  return title.replace(regex, '').trim();
+}
+
+// converts ISO8608 time to seconds
+function iso8601DurationToSeconds(duration) {
+  const parsedDuration = moment.duration(duration);
+  return parsedDuration.asSeconds();
+}
 
 async function handleJoin(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+  // check if user is already in db
+  // if so, continue
+  // if not, return with link
+  const { data:findPlayer, error:findPlayerError } = await supabase
+    .from('players')
+    .select('name')
+    .eq('name', context['display-name'])
+    .maybeSingle();
+  if(findPlayerError) {
+    console.log(`Error getting users: ${error}`);
+  } else if(findPlayer === null) {
+    console.log('No users returned.');
+    client.say(channel, `@${context['display-name']}, you have not linked your Twitch account with Spin The Wheel yet. Please visit https://go.jasper.stream/stw to link your account.`)
+  } 
+
+  // now that they're signed in, change their is_player status to true
+  const { data:setUserAsPlayer, error:setUserAsPlayerError } = await supabase
+    .from('players')
+    .update({ is_player: true })
+    .eq('name', context['display-name'])
+    .select();
+
+  if(setUserAsPlayerError) {
+    console.log(`Error setting ${context['display-name']} as player.`);
+  } else if(setUserAsPlayer[0].is_player === true) {
+    client.say(channel, `@${context['display-name']}, you have already joined. Type !leave if you want to leave the queue. You will get called out in chat and by Jasper when it's your time to play.`)
+  } else {
+    client.say(channel, `@${context['display-name']}, you have joined the queue of Spin The Wheel. Type !leave to leave the queue.`);
+  }
 }
-async function handleSR(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+async function handleR(channel, command, context, msg) {
+  
+  const beforeFilter = msg.toString().split(" ")[1];
+  const result = youtubeCleaner.execute(beforeFilter);
+  if(result.requestHasFuckedUpLink || result === null) {
+    client.say(channel, `@${context['display-name']}, you can only request songs with a YouTube link, nothing else.`);
+  } else if(result.filteredYouTubeLink) {
+
+    async function getYouTubeVideo(videoId) {
+      const vidId = videoId || null;
+      const response = vidId ? await fetch(`${process.env.YOUTUBE_API_URL}?part=snippet%2CcontentDetails&id=${vidId}&maxResults=1&key=${process.env.YOUTUBE_API_KEY}`) : null;
+      const yt = await response.json();
+      /*
+        items[0].id // 3hRPWmWftlY
+        items[0].snippet.title // Miggs de Bruijn - Hopen Dat 't Lukt Ft. Zack Ink (Prod. ATLouis)
+        items[0].snippet.thumbnails.standard.url // https://i.ytimg.com/vi/3hRPWmWftlY/sddefault.jpg
+        items[0].snippet.thumbnails.high.url // https://i.ytimg.com/vi/3hRPWmWftlY/hqdefault.jpg
+        items[0].snippet.thumbnails.maxres.url // https://i.ytimg.com/vi/3hRPWmWftlY/maxresdefault.jpg
+        items[0].contentDetails.duration // PT3M25S
+      */
+      const title = cleanTitle(yt.items[0].snippet.title); // this outputs the video title
+      const seconds = iso8601DurationToSeconds(yt.items[0].contentDetails.duration);
+      client.say(channel, title);
+      console.log(seconds); // 185, for example
+    }
+
+    await getYouTubeVideo(result.filteredYouTubeLink);
+
+  } else {
+    client.say(channel, `@${context['display-name']}, you can only request songs with a YouTube link, nothing else.`);
+  }
+
 }
 async function handleVote(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+  client.say(channel, `${command} is called by ${context['display-name']}`); 
 }
 async function handleNew(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+  client.say(channel, `${command} is called by ${context['display-name']}`); 
 }
-async function handle69(channel, command, context) {
-  client.say(channel, `nice.`); 
+async function handle69(channel) {
+  client.say(channel, 'Kappa');
 }
 async function handleLeave(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+  client.say(channel, `${command} is called by ${context['display-name']}`); 
 }
 async function handlePos(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+  client.say(channel, `${command} is called by ${context['display-name']}`); 
 }
 async function handleVotewhen(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+  client.say(channel, `${command} is called by ${context['display-name']}`); 
 }
 async function handleGetsong(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+  client.say(channel, `${command} is called by ${context['display-name']}`); 
 }
 async function handleSongs(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+  client.say(channel, `${command} is called by ${context['display-name']}`); 
 }
 async function handleStats(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+  client.say(channel, `${command} is called by ${context['display-name']}`); 
 }
 async function handleVs(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+  client.say(channel, `${command} is called by ${context['display-name']}`); 
 }
 async function handleCat(channel, command, context) {
-  client.say(channel, `${command} is called by ${context.username}`); 
+  client.say(channel, `${command} is called by ${context['display-name']}`); 
+}
+async function handleBeep(channel, command, context) {
+  console.log(`${command} received!`);
+  client.say(channel, 'boop!');
 }
 
 // Express server for Render port binding
